cdc6e9a2ee54a5023e58c2dd1a2773ec01014264
Catalin <csiegling@gmail.com>
2022-05-29 13:07:56 -0500

    changed to switch and darkmoded

--- server.js
+++ server.js
@@ -8,35 +8,28 @@ const server = http.createServer((req, res) => {
     const page = url.parse(req.url).pathname;
     const params = querystring.parse(url.parse(req.url).query);
     console.log(page);
-    if (page == '/') {
-        fs.readFile('index.html', function (err, data) {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+    const readWrite = (file, contentType) => {
+        fs.readFile(file, function (err, data) {
+            res.writeHead(200, { 'Content-Type': contentType });
             res.write(data);
             res.end();
         });
     }
-    else if (page == '/css/style.css') {
-        fs.readFile('css/style.css', function (err, data) {
-            res.write(data);
-            res.end();
-        });
-    } else if (page == '/js/main.js') {
-        fs.readFile('js/main.js', function (err, data) {
-            res.writeHead(200, { 'Content-Type': 'text/javascript' });
-            res.write(data);
-            res.end();
-        });
-    } else {
-        figlet('404!!', function (err, data) {
-            if (err) {
-                console.log('Something went wrong...');
-                console.dir(err);
-                return;
-            }
-            res.write(data);
-            res.end();
-        });
+    switch (page) {
+        case '/':
+            readWrite('index.html', 'text/html')
+            break;
+        case '/css/style.css':
+            fs.readFile('css/style.css', function (err, data) {
+                res.write(data);
+                res.end();
+            });
+            break;
+        case '/js/main.js':
+            readWrite('js/main.js', 'text/javascript')
+            break;
     }
 });
+
 
 server.listen(8000);
